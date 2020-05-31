584f45aea182ae887e0aef09ceae77dcdc259040
yinlong22 <342241857@.com>
2020-06-01 02:39:00 +0800

    deploy

---
+++ precache-manifest.cb7d79741f4d5e316d1b2f2a3a526ca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004a3951ab2fd39cd9ea",
+    "url": "/muconey-1-website/css/app.a7ad9221.css"
+  },
+  {
+    "revision": "df89f267e492e91134cf99a519683ed6",
+    "url": "/muconey-1-website/index.html"
+  },
+  {
+    "revision": "004a3951ab2fd39cd9ea",
+    "url": "/muconey-1-website/js/app.46ecac9e.js"
+  },
+  {
+    "revision": "ef59bc621b5d0c8ff038",
+    "url": "/muconey-1-website/js/chunk-vendors.3381fec9.js"
+  },
+  {
+    "revision": "4d99c8c836f5ec58113b01c673004c6a",
+    "url": "/muconey-1-website/manifest.json"
+  },
+  {
+    "revision": "7fc757b5a80c266bf8b6b49263384af9",
+    "url": "/muconey-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/muconey-1-website/robots.txt"
+  }
+]);
